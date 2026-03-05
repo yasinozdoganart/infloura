822abb0847f1a4c354d21decf2c3e64ead8d8f9f
Yasin Özdoğan <yasinozdoganart@icloud.com>
2026-03-05 15:33:06 +0300

    chore: integrate @vercel/speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Script from "next/script";
 
 const geistSans = Geist({
@@ -54,6 +55,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
         <Toaster />
       </body>
     </html>
